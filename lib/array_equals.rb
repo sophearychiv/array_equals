e7abf499ec89ef5fa58b18957f6d7575b863e526
Sopheary Chiv <sophearychiv@gmail.com>
2019-02-24 16:24:27 -0800

    simplified conditionals

--- lib/array_equals.rb
+++ lib/array_equals.rb
@@ -1,18 +1,14 @@
 # Determines if the two input arrays have the same count of elements
 # and the same integer values in the same exact order
 def array_equals(array1, array2)
-  if (array1 == [] && array2 == []) || (array1 == nil && array2 == nil)
-    return true
-  elsif (array1 == nil && array2 != nil) || (array1 != nil && array2 == nil)
+  return true if (array1 == [] && array2 == []) || (array1 == nil && array2 == nil)
+  return false if array1 == nil || array2 == nil
+  if array1.length != array2.length
     return false
-  elsif array1.length == array2.length
+  else
     array1.length.times do |index|
-      if array1[index] != array2[index]
-        return false
-      end
+      return false if array1[index] != array2[index]
     end
     return true
-  else
-    return false
   end
 end
